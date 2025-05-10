d69bee55dd65da0bebc087b89d9c6c387170870d
Rayhan Akash <86167036+Aka5h23@users.noreply.github.com>
2025-05-10 07:17:47 +0600

    Added data fetching from backend (#1)

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from Express2!" });
   //res.send("Hello");
 });
+
+app.get("/api/products", (req, res) => {
+    res.json([
+      { id: 1, name: "iPhone 15 Pro", price: 1299 },
+      { id: 2, name: "Galaxy S24 Ultra", price: 1199 },
+    ]);
+  });
 
 const PORT = process.env.PORT || 5000;
 
